61560381ec3c1b328ddd198d1dbf5acf738050f8
Pratik Vyavahare <89389983+Pratikv2002@users.noreply.github.com>
2025-12-31 23:22:48 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2916,7 +2916,7 @@ app.get('/api/revenue', authenticateToken, async (req, res) => {
 // ---------------------------------------------
 // START SERVER
 // ---------------------------------------------
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
